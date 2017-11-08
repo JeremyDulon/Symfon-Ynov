3d962aa7685723f0efafe16883292c445befae8c
Lykaos <jeremy.dulon@live.fr>
2017-11-08 11:40:37 +0100

    Deployment file

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+rm -r var/cache
+mkdir var/cache
+chown -R www-data:www-data var/cache
+chmod -R 777 var/cache var/logs
+chmod -R 777 var/cache var/logs
+bin/console assets:install --symlink
+chmod -R 777 var/cache var/logs
+bin/console doctrine:cache:clear-metadata
+bin/console doctrine:cache:clear-query
+bin/console doctrine:cache:clear-result

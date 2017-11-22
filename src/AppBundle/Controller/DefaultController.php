61c1cc8f2e9307caf6cf82e58d3df91665fd29c2
clementcarda <clement.cardarelli@ynov.com>
2017-11-22 09:38:14 +0100

    routes, controllers, vue pour sport, joueur, club

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,11 +21,10 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $sports = $em->getRepository('AppBundle:Sport')->findAll();
-        return $this->render('AppBundle::sport.html.twig', [
+        return $this->render('AppBundle::sports.html.twig', [
             'sports' => $sports
         ]);
     }
-
     public function showSportAction(Request $request)
     {
         $id = $request->get('id');
@@ -33,6 +32,42 @@ class DefaultController extends Controller
         $sport = $em->getRepository('AppBundle:Sport')->find($id);
         return $this->render('AppBundle::showSport.html.twig', [
             'sport' => $sport
+        ]);
+    }
+
+    public function getJoueursAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $joueurs = $em->getRepository('AppBundle:Joueur')->findAll();
+        return $this->render('AppBundle::joueurs.html.twig', [
+            'joueurs' => $joueurs
+        ]);
+    }
+    public function showJoueurAction(Request $request)
+    {
+        $id = $request->get('id');
+        $em = $this->getDoctrine()->getManager();
+        $joueur = $em->getRepository('AppBundle:Joueur')->find($id);
+        return $this->render('AppBundle::showJoueur.html.twig', [
+            'joueur' => $joueur
+        ]);
+    }
+
+    public function getClubsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $clubs = $em->getRepository('AppBundle:Club')->findAll();
+        return $this->render('AppBundle::clubs.html.twig', [
+            'clubs' => $clubs
+        ]);
+    }
+    public function showClubAction(Request $request)
+    {
+        $id = $request->get('id');
+        $em = $this->getDoctrine()->getManager();
+        $club = $em->getRepository('AppBundle:Club')->find($id);
+        return $this->render('AppBundle::showClub.html.twig', [
+            'club' => $club
         ]);
     }
 }
